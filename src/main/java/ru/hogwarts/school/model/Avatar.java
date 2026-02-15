21586f3f6ff6257b1791c6dd06928dbd4ad104de
Aleksey <d_alesha@rambler.ru>
2026-02-15 17:10:47 +0300

    Добавление решения

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -13,7 +13,10 @@ public class Avatar {
     private String filePath;
     private long fileSize;
     private String mediaType;
+
+    @Lob
     private byte[] data;
+
     @OneToOne
     private Student student;
 
