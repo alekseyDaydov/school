f943698e1d3159199f1afc6b5a30f1a92909a4db
d_alesha <d_alesha@rambler.ru>
2026-02-28 21:37:53 +0300

    добавление решения

--- src/main/resources/liquibase/scripts/user.sql
+++ src/main/resources/liquibase/scripts/user.sql
@@ -1,1 +1,7 @@
 -- liquibase formatted sql
+
+-- changeset a.davydov:1
+CREATE INDEX idx_student_name ON student (name)
+
+-- changeset a.davydov:2
+CREATE INDEX idx_faculty_name_color ON faculty (name, color)
