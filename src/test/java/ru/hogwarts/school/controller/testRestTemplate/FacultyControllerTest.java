962ee1f0f0d7dc0afeaf4960f508350f33ccbd56
d_alesha <d_alesha@rambler.ru>
2026-03-09 11:04:44 +0300

    добавление решения

--- src/test/java/ru/hogwarts/school/controller/testRestTemplate/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/testRestTemplate/FacultyControllerTest.java
@@ -34,187 +34,187 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("RestTemplate FacultyController")
 public class FacultyControllerTest {
-//
-//    @LocalServerPort
-//    int port;
-//
-//    @Autowired
-//    private TestRestTemplate testRestTemplate;
-//
-//    @Autowired
-//    private FacultyController facultyController;
-//
-//    @Autowired
-//    private FacultyRepository facultyRepository;
-//
-//    @Autowired
-//    private StudentRepository studentRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        //очищаю базу перед каждым тестом
-//        studentRepository.deleteAll();
-//        facultyRepository.deleteAll();
-//    }
-//
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private FacultyController facultyController;
+
+    @Autowired
+    private FacultyRepository facultyRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @BeforeEach
+    void setUp() {
+        //очищаю базу перед каждым тестом
+        studentRepository.deleteAll();
+        facultyRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Проверка что контроллер не пустой и запускается")
+    void contextLoads() throws Exception {
+        Assertions.assertThat(facultyController).isNotNull();
+    }
+
+    @Test
+    @DisplayName("POST - Добавление факультета")
+    void testCreateFaculty() throws Exception {
+        String url = "http://localhost:" + port + "/faculty";
+        Faculty faculty = new Faculty(0L,"It", "red", new ArrayList<>());
+        Faculty addFaculty = this.testRestTemplate.postForObject(url, faculty, Faculty.class);
+
+//       Проверяю, что не добавлен пустой объект
+        Assertions
+                .assertThat(addFaculty)
+                .isNotNull();
+
+        // Проверяем в базе, что факультет добавился
+        assertTrue(facultyRepository.findById(addFaculty.getId()).isPresent());
+    }
+
+    @Test
+    @DisplayName("GET Поиск факультета по Id -факультета")
+    void testGetFacultyInfoByIdFaculty() throws Exception {
+
+        Faculty faculty = new Faculty();
+        faculty.setName("it");
+        faculty.setColor("red");
+
+        //создаю запись
+        Faculty addFaculty = facultyController.createFaculty(faculty);
+        String url = "http://localhost:" + port + "/faculty/" + addFaculty.getId();
+
+        Faculty getFaculty = this.testRestTemplate.getForObject(url, Faculty.class);
+
+        Assertions.assertThat(getFaculty).isNotNull();
+        Assertions.assertThat(addFaculty.getId()).isEqualTo(getFaculty.getId());
+    }
+
+
+    @Test
+    @DisplayName("PUT Обновление данных по факультету")
+    void tesUpdateFaculty() throws Exception {
+        String url = "http://localhost:" + port + "/faculty";
+        Faculty faculty = new Faculty(0, "ItFaculty", "blue", new ArrayList<>());
+        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
+
+//        обновляю данные факультета
+        Faculty updateFaculty = new Faculty( );
+        updateFaculty.setId(createdFaculty.getId());
+        updateFaculty.setName("OIT");
+        updateFaculty.setColor("red");
+        // обновили данные через запрос в БД
+        testRestTemplate.put(url, updateFaculty);
+
+//      получил ответ и проверили
+        ResponseEntity<Faculty> response = testRestTemplate.getForEntity(url + "/" + createdFaculty.getId(), Faculty.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertThat(updateFaculty.getName()).isEqualTo(response.getBody().getName());
+        Assertions.assertThat(updateFaculty.getColor()).isEqualTo(response.getBody().getColor());
+
+    }
+
+    @Test
+    @DisplayName("Delete Удаление данных по факультету")
+    void testDeleteFaculty() throws Exception {
+        String url = "http://localhost:" + port + "/faculty";
+        Faculty faculty = new Faculty(0L, "ItFaculty", "blue",new ArrayList<>());
+//        создали
+        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
+        url = "http://localhost:" + port + "/faculty/" + createdFaculty.getId();
+
+//       выполнили запрос на удаление проверили ответ
+        ResponseEntity<Faculty> response = testRestTemplate.exchange(url, HttpMethod.DELETE, HttpEntity.EMPTY, Faculty.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+//        проверили, что записи нет
+        Faculty deleteFaculty = this.testRestTemplate.getForObject(url, Faculty.class);
+        Assertions.assertThat(deleteFaculty).isNull();
+    }
+
+    @Test
+    @DisplayName("Get- Поиск по цвету")
+    void testFilterColorByColor() throws Exception {
+
+//        создали 2 записи в БД
+        Faculty faculty = new Faculty( 0L,"ItFaculty", "blue", new ArrayList<>());
+        Faculty faculty2 = new Faculty(0L, "OIT", "blue", new ArrayList<>());
+        String url = "http://localhost:" + port + "/faculty";
+        // добавили
+        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
+        Faculty createdFaculty2 = testRestTemplate.postForObject(url, faculty2, Faculty.class);
+
+        url = "http://localhost:" + port + "/faculty?color=" + createdFaculty.getColor();
+
+//       Запрос помска цвета
+        ResponseEntity<Collection<Faculty>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Faculty>>() {
+        });
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Collection<Faculty> jsonObjects = response.getBody();
+        Assertions.assertThat(jsonObjects)
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(List.of(createdFaculty, createdFaculty2));
+    }
+
+    @Test
+    @DisplayName("Get- Поиск по имени или цвету")
+    void testFilterColorByColorOrName() throws Exception {
+        //        создали 2 записи в БД
+        final String FIND_ORANGE = "orange";
+        Faculty faculty = new Faculty(0L,"ItOrange", "blue", new ArrayList<>());
+        Faculty faculty2 = new Faculty(0L, "Blue", "orange", new ArrayList<>());
+        String url = "http://localhost:" + port + "/faculty";
+
+        // добавили
+        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
+        Faculty createdFaculty2 = testRestTemplate.postForObject(url, faculty2, Faculty.class);
+
+        url = "http://localhost:" + port + "/faculty?find=" + FIND_ORANGE;
+
+//       Запрос помска цвета
+        ResponseEntity<Collection<Faculty>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Faculty>>() {
+        });
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Collection<Faculty> jsonObjects = response.getBody();
+        Assertions.assertThat(jsonObjects)
+                .isNotNull()
+                .isNotEmpty()
+                .isEqualTo(List.of(createdFaculty, createdFaculty2));
+    }
+
 //    @Test
-//    @DisplayName("Проверка что контроллер не пустой и запускается")
-//    void contextLoads() throws Exception {
-//        Assertions.assertThat(facultyController).isNotNull();
-//    }
+//    @DisplayName("Get- Поиск  факультета по id студента")
+//    void testFilterColorIdStudent() throws Exception {
+//        Faculty faculty = new Faculty(0,"It", "blue",new ArrayList<>());
+//        Student student = new Student(0,"Pety", 25, null);  // Без id и faculty!
 //
-//    @Test
-//    @DisplayName("POST - Добавление факультета")
-//    void testCreateFaculty() throws Exception {
-//        String url = "http://localhost:" + port + "/faculty";
-//        Faculty faculty = new Faculty(0L,"It", "red", new ArrayList<>());
-//        Faculty addFaculty = this.testRestTemplate.postForObject(url, faculty, Faculty.class);
+//        Faculty savedFaculty = facultyRepository.save(faculty);
 //
-////       Проверяю, что не добавлен пустой объект
-//        Assertions
-//                .assertThat(addFaculty)
-//                .isNotNull();
+//        savedFaculty.getStudents().add(student);
 //
-//        // Проверяем в базе, что факультет добавился
-//        assertTrue(facultyRepository.findById(addFaculty.getId()).isPresent());
-//    }
+//        Student createdStudent = studentRepository.save(student);  // Каскад работает!
+//        createdStudent.setFaculty(savedFaculty);
 //
-//    @Test
-//    @DisplayName("GET Поиск факультета по Id -факультета")
-//    void testGetFacultyInfoByIdFaculty() throws Exception {
-//
-//        Faculty faculty = new Faculty();
-//        faculty.setName("it");
-//        faculty.setColor("red");
-//
-//        //создаю запись
-//        Faculty addFaculty = facultyController.createFaculty(faculty);
-//        String url = "http://localhost:" + port + "/faculty/" + addFaculty.getId();
-//
-//        Faculty getFaculty = this.testRestTemplate.getForObject(url, Faculty.class);
-//
-//        Assertions.assertThat(getFaculty).isNotNull();
-//        Assertions.assertThat(addFaculty.getId()).isEqualTo(getFaculty.getId());
-//    }
-//
-//
-//    @Test
-//    @DisplayName("PUT Обновление данных по факультету")
-//    void tesUpdateFaculty() throws Exception {
-//        String url = "http://localhost:" + port + "/faculty";
-//        Faculty faculty = new Faculty(0, "ItFaculty", "blue", new ArrayList<>());
-//        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
-//
-////        обновляю данные факультета
-//        Faculty updateFaculty = new Faculty( );
-//        updateFaculty.setId(createdFaculty.getId());
-//        updateFaculty.setName("OIT");
-//        updateFaculty.setColor("red");
-//        // обновили данные через запрос в БД
-//        testRestTemplate.put(url, updateFaculty);
-//
-////      получил ответ и проверили
-//        ResponseEntity<Faculty> response = testRestTemplate.getForEntity(url + "/" + createdFaculty.getId(), Faculty.class);
+//        String url1 = "http://localhost:" + port + "/faculty?idStudent=" + createdStudent.getId();
+//        ResponseEntity<List<Faculty>> response = this.testRestTemplate.exchange(url1, HttpMethod.GET, null,
+//                new ParameterizedTypeReference<List<Faculty>>() {});
 //
 //        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        Assertions.assertThat(updateFaculty.getName()).isEqualTo(response.getBody().getName());
-//        Assertions.assertThat(updateFaculty.getColor()).isEqualTo(response.getBody().getColor());
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Delete Удаление данных по факультету")
-//    void testDeleteFaculty() throws Exception {
-//        String url = "http://localhost:" + port + "/faculty";
-//        Faculty faculty = new Faculty(0L, "ItFaculty", "blue",new ArrayList<>());
-////        создали
-//        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
-//        url = "http://localhost:" + port + "/faculty/" + createdFaculty.getId();
-//
-////       выполнили запрос на удаление проверили ответ
-//        ResponseEntity<Faculty> response = testRestTemplate.exchange(url, HttpMethod.DELETE, HttpEntity.EMPTY, Faculty.class);
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//
-////        проверили, что записи нет
-//        Faculty deleteFaculty = this.testRestTemplate.getForObject(url, Faculty.class);
-//        Assertions.assertThat(deleteFaculty).isNull();
-//    }
-//
-//    @Test
-//    @DisplayName("Get- Поиск по цвету")
-//    void testFilterColorByColor() throws Exception {
-//
-////        создали 2 записи в БД
-//        Faculty faculty = new Faculty( 0L,"ItFaculty", "blue", new ArrayList<>());
-//        Faculty faculty2 = new Faculty(0L, "OIT", "blue", new ArrayList<>());
-//        String url = "http://localhost:" + port + "/faculty";
-//        // добавили
-//        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
-//        Faculty createdFaculty2 = testRestTemplate.postForObject(url, faculty2, Faculty.class);
-//
-//        url = "http://localhost:" + port + "/faculty?color=" + createdFaculty.getColor();
-//
-////       Запрос помска цвета
-//        ResponseEntity<Collection<Faculty>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Faculty>>() {
-//        });
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//
-//        Collection<Faculty> jsonObjects = response.getBody();
-//        Assertions.assertThat(jsonObjects)
+//        Assertions.assertThat(response.getBody())
 //                .isNotNull()
-//                .isNotEmpty()
-//                .isEqualTo(List.of(createdFaculty, createdFaculty2));
+//                .isEqualTo(List.of(savedFaculty));
 //    }
-//
-//    @Test
-//    @DisplayName("Get- Поиск по имени или цвету")
-//    void testFilterColorByColorOrName() throws Exception {
-//        //        создали 2 записи в БД
-//        final String FIND_ORANGE = "orange";
-//        Faculty faculty = new Faculty(0L,"ItOrange", "blue", new ArrayList<>());
-//        Faculty faculty2 = new Faculty(0L, "Blue", "orange", new ArrayList<>());
-//        String url = "http://localhost:" + port + "/faculty";
-//
-//        // добавили
-//        Faculty createdFaculty = testRestTemplate.postForObject(url, faculty, Faculty.class);
-//        Faculty createdFaculty2 = testRestTemplate.postForObject(url, faculty2, Faculty.class);
-//
-//        url = "http://localhost:" + port + "/faculty?find=" + FIND_ORANGE;
-//
-////       Запрос помска цвета
-//        ResponseEntity<Collection<Faculty>> response = testRestTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Collection<Faculty>>() {
-//        });
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//
-//        Collection<Faculty> jsonObjects = response.getBody();
-//        Assertions.assertThat(jsonObjects)
-//                .isNotNull()
-//                .isNotEmpty()
-//                .isEqualTo(List.of(createdFaculty, createdFaculty2));
-//    }
-//
-////    @Test
-////    @DisplayName("Get- Поиск  факультета по id студента")
-////    void testFilterColorIdStudent() throws Exception {
-////        Faculty faculty = new Faculty(0,"It", "blue",new ArrayList<>());
-////        Student student = new Student(0,"Pety", 25, null);  // Без id и faculty!
-////
-////        Faculty savedFaculty = facultyRepository.save(faculty);
-////
-////        savedFaculty.getStudents().add(student);
-////
-////        Student createdStudent = studentRepository.save(student);  // Каскад работает!
-////        createdStudent.setFaculty(savedFaculty);
-////
-////        String url1 = "http://localhost:" + port + "/faculty?idStudent=" + createdStudent.getId();
-////        ResponseEntity<List<Faculty>> response = this.testRestTemplate.exchange(url1, HttpMethod.GET, null,
-////                new ParameterizedTypeReference<List<Faculty>>() {});
-////
-////        assertEquals(HttpStatus.OK, response.getStatusCode());
-////        Assertions.assertThat(response.getBody())
-////                .isNotNull()
-////                .isEqualTo(List.of(savedFaculty));
-////    }
 }
 
 
