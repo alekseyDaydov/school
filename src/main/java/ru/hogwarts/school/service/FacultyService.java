2a6b73eb44f0948a0b11fe6befb84f1ffed38d3f
Aleksey <d_alesha@rambler.ru>
2026-03-10 13:09:43 +0300

    Добавление аннотации @Slf4j от lombok

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.apache.logging.log4j.LogManager;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,10 @@ import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
-import java.util.List;
-
 @Service
+@Slf4j
 public class FacultyService {
-    Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     String CREATE_TEXT_INFO = "Was invoked method for {}";
     String CREATE_TEXT_ERROR = "There is not student with id = {}";
 
@@ -23,42 +24,42 @@ public class FacultyService {
 
     //    Create
     public Faculty createFaculty(Faculty faculty) {
-        logger.info(CREATE_TEXT_INFO, "createFaculty");
+        log.info(CREATE_TEXT_INFO, "createFaculty");
         return facultyRepository.save(faculty);
     }
 
     //    read
     public Faculty findFaculty(long id) {
-        logger.info(CREATE_TEXT_INFO, "findFaculty");
-        logger.error(CREATE_TEXT_ERROR, id);
+        log.info(CREATE_TEXT_INFO, "findFaculty");
+        log.error(CREATE_TEXT_ERROR, id);
         return facultyRepository.findById(id).orElse(null);
     }
 
     //update
     public Faculty updateFaculty(Faculty faculty) {
-        logger.info(CREATE_TEXT_INFO, "updateFaculty");
+        log.info(CREATE_TEXT_INFO, "updateFaculty");
         return facultyRepository.save(faculty);
     }
 
     //delete
     public void deleteFaculty(long id) {
-        logger.info(CREATE_TEXT_INFO, "deleteFaculty");
-        logger.error(CREATE_TEXT_ERROR, id);
+        log.info(CREATE_TEXT_INFO, "deleteFaculty");
+        log.error(CREATE_TEXT_ERROR, id);
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> filterColor(String color) {
-        logger.info(CREATE_TEXT_INFO, "filterColor");
+        log.info(CREATE_TEXT_INFO, "filterColor");
         return facultyRepository.findByColor(color);
     }
 
     public Collection<Faculty> getAll() {
-        logger.info(CREATE_TEXT_INFO, "getAll");
+        log.info(CREATE_TEXT_INFO, "getAll");
         return facultyRepository.findAll();
     }
 
     public Collection<Faculty> findByNameContainsIgnoreCaseOrColorIgnoreCase(String find) {
-        logger.info(CREATE_TEXT_INFO, "findByNameContainsIgnoreCaseOrColorIgnoreCase");
+        log.info(CREATE_TEXT_INFO, "findByNameContainsIgnoreCaseOrColorIgnoreCase");
         return facultyRepository.findFacultyByNameContainingIgnoreCaseOrColorContainingIgnoreCase(find, find);
     }
 }
